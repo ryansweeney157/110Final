1c755c8fc896890475fda01e1e5cdfd3b8f3fb28
RyanS <ryanweeney157@gmail.com>
2024-10-30 21:08:19 -0400

    sorry forgot to commit for a while

--- java.js
+++ java.js
@@ -1,10 +1,19 @@
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('header');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
 $(function () {
     let headerElem = $('header');
     let logo = $('#logo');
     let navMenu = $('#nav-menu');
     let navToggle = $('#nav-toggle');
 
-   $('#properties-slider').slick({
+    $('#properties-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
@@ -35,50 +44,5 @@ $(function () {
                 }
             }
         ]
-   });
-
-   $('#testimonials-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<a href="#" class="slick-arrow slick-prev"><</a>',
-        nextArrow: '<a href="#" class="slick-arrow slick-next">></a>'
-   });
-
-   navToggle.on('click', () => {
-       navMenu.css('right', '0');
-   });
-
-   $('#close-flyout').on('click', () => {
-        navMenu.css('right', '-100%');
-   });
-
-   $(document).on('click', (e) => {
-       let target = $(e.target);
-       if (!(target.closest('#nav-toggle').length > 0 || target.closest('#nav-menu').length > 0)) {
-           navMenu.css('right', '-100%');
-       }
-   });
-
-   $(document).scroll(() => {
-       let scrollTop = $(document).scrollTop();
-
-       if (scrollTop > 0) {
-           navMenu.addClass('is-sticky');
-           logo.css('color', '#000');
-           headerElem.css('background', '#fff');
-           navToggle.css('border-color', '#000');
-           navToggle.find('.strip').css('background-color', '#000');
-       } else {
-           navMenu.removeClass('is-sticky');
-           logo.css('color', '#fff');
-           headerElem.css('background', 'transparent');
-           navToggle.css('bordre-color', '#fff');
-           navToggle.find('.strip').css('background-color', '#fff');
-       }
-
-       headerElem.css(scrollTop >= 200 ? {'padding': '0.5rem', 'box-shadow': '0 -4px 10px 1px #999'} : {'padding': '1rem 0', 'box-shadow': 'none' });
-   });
-
-   $(document).trigger('scroll');
-});
+    }); // Closing parenthesis added here
+}); // Added closing parenthesis for the document ready function
